5101e564cd974264cccb8e15e6f59801115e18be
Derek Vawdrey <derek.vawdrey@gmail.com>
2026-02-10 14:51:04 -0700

    Adding in error handling if any controller throws

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import router from "./routes";
 import config from "./config/config";
+import { Request, Response, NextFunction } from "express";
 
 dotenv.config();
 
@@ -13,3 +14,22 @@ app.use("/api", router);
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
 });
+
+app.use((
+    err: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) => {
+    console.error(err);
+
+    res.status(500).json({
+        message: err.message || "Internal server error",
+    });
+});
+
+app.get("/health", (_req, res) => {
+    res.status(200).send("ok");
+});
+
+export default app;
