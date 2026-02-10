3412e108166d4c37cea4b6f02893b7d7ce30469e
Derek Vawdrey <derek.vawdrey@gmail.com>
2026-02-10 13:51:49 -0700

    Adding containers for merge and company module

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -2,9 +2,12 @@
  * This file enables dependency injection.
  */
 import { Container } from "inversify";
+import { mergeContainersModule } from "../modules/merge/di/container.module";
+import { companyContainersModule } from "../modules/company/di/container.module";
 
 const container = new Container();
 
 container.load(
-
+    companyContainersModule,
+    mergeContainersModule
 );
---
+++ src/modules/company/di/container.module.ts
@@ -0,0 +1,7 @@
+import { ContainerModule, ContainerModuleLoadOptions } from "inversify";
+
+export const companyContainersModule: ContainerModule = new ContainerModule(
+    (options: ContainerModuleLoadOptions) => {
+
+    }
+)
